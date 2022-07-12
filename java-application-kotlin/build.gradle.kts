7ad533cd263a4cfb2ef0a8cc2653e133e6847a9f
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-07-12 23:40:39 +0200

    :sparkles: SourceSets, Dependencies & MainClassName

--- java-application-kotlin/build.gradle.kts
+++ java-application-kotlin/build.gradle.kts
@@ -1,5 +1,28 @@
 plugins {
-    java
-    `java-library`
+//    java
+//    `java-library`
     application
 }
+
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src"))
+        }
+    }
+    test {
+        java {
+            setSrcDirs(listOf("test/src"))
+        }
+    }
+}
+
+application {
+    mainClass.set("be.dog.d.steven.Repository")
+}
+
+dependencies {
+    implementation(files ("lib/log4j-1.2.8.jar", "lib/junit-3.8.1.jar", "lib/jaxb-api-2.3.1.jar"))
+}
+
+version = "1.0-SNAPSHOT"
